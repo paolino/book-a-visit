738987520598c4f84e785e65c52aa4320cbb0da6
paolino <paolo.veronelli@gmail.com>
2017-04-21 10:11:40 +0200

    more instances for  Naive

--- Naive.hs
+++ Naive.hs
@@ -11,14 +11,17 @@
 {-# language ConstraintKinds #-}
 {-# language ExistentialQuantification #-}
 {-# language UndecidableInstances #-}
+{-# language DeriveAnyClass #-}
 
 module Naive where
 
 import Model
-
+import qualified Data.Set as S
+import Data.Set (Set)
 import Data.IntMap (IntMap)
 import qualified Data.IntMap as M
 import Spans
+import Geometry
 
 data Naive
 
@@ -45,25 +48,42 @@ type Distance = Float
 distance (x,y) (x',y') = sqrt ((x - x') ^ 2 + (y - y') ^ 2)
 
 data instance Place Naive = Place Pos deriving Eq
-data instance Zone Naive = Zone Pos Distance deriving Eq
+data instance Zone Naive = Zone Pos Distance | NullZone deriving Eq
 
 instance Include (Zone Naive) (Zone Naive) where
   Zone p d `include` Zone p' d' = d > d' && distance p p' < d
+  x `include` NullZone = True
+  NullZone `include` _ = False
 
 instance Include (Zone Naive) (Place Naive) where
   Zone p d `include` Place p' = distance p p' < d
+  NullZone `include` _ = False
+
+instance Monoid (Zone Naive) where
+  mempty = NullZone
+  NullZone `mappend` x = x
+  x `mappend` NullZone = x
+  Zone p d `mappend` Zone p' d' = uncurry Zone $ encloser (p,d) (p',d')
 
 instance Include (Slot Naive) (Slot Naive) where
   x `include` y  = (x `mappend` y) == x
 
-data instance Match (User Naive) = MatchUser String
-data instance Match (Vet Naive) = MatchVet String
+data instance Match (User Naive) = MatchUser (Set String)
+data instance Match (Vet Naive) = MatchVet (Set String)
 
 instance Include (Match (User Naive)) (User Naive) where
-  MatchUser x `include` User y = x == y
+  MatchUser x `include` User y = y `S.member` x
 
 instance Include (Match (Vet Naive)) (Vet Naive) where
-  MatchVet x `include` Vet y = x == y
+  MatchVet x `include` Vet y = y `S.member` x
+
+instance Monoid (Match (User Naive)) where
+  MatchUser x `mappend` MatchUser y = MatchUser $ x `mappend` y
+  mempty = MatchUser mempty
+
+instance Monoid (Match (Vet Naive)) where
+  MatchVet x `mappend` MatchVet y = MatchVet $ x `mappend` y
+  mempty = MatchVet mempty
 
 instance Modify IntMap where
   data Ix IntMap  = Ix Int
